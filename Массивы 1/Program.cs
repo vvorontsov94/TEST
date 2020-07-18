744741c7412436fced6865a2f8506e22586bb31e
Владимир Воронцов <vvorontsov@bk.ru>
2020-07-18 21:46:57 +0300

    +массивы

---
+++ Массивы 1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Массивы_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                int n, sum, min;
+                sum = 0;
+                int[] MYarray;
+
+                Console.WriteLine("Введи размер массива");
+                Console.WriteLine();
+
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+
+                    MYarray = new int[n];
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine("Введи элемент " + i + " массива");
+                        MYarray[i] = int.Parse(Console.ReadLine());
+                    }
+
+                }
+
+                catch(Exception)
+                {
+                    Console.WriteLine("ЗАЛУПА");
+                    Console.ReadLine();
+                    continue;
+
+                }
+
+
+                Console.WriteLine();
+                Console.WriteLine("Вывод массива");
+                for(int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Элемент " + i + ": " + MYarray[i]);
+                }
+
+
+                Console.WriteLine();
+                Console.WriteLine("Вывод массива в обратном порядке");
+                for (int i = MYarray.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine("Элемент " + i + ": " + MYarray[i]);
+                }
+
+                Console.WriteLine();
+                for (int i = 0; i < n; i++)
+                {
+                    if (MYarray[i] % 2 == 0)
+                    {
+                        sum = sum + MYarray[i];
+                    }
+                }
+                Console.WriteLine("Сумма четных чисел в массиве: " + sum);
+
+                Console.WriteLine();
+                min = MYarray[0];
+                for (int i = 0; i < n; i++)
+                {
+                    if (MYarray[i] < min)
+                    {
+                        min = MYarray[i];
+                    }
+                }
+                Console.WriteLine("Минимальный элемент массива: " + min);
+
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
