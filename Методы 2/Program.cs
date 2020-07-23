1e7321646a224d7d8716dd5e06fb6126b2fdc78a
Владимир Воронцов <vvorontsov@bk.ru>
2020-07-23 21:24:59 +0300

    Добавлены методы

---
+++ Методы 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Методы_2
+{
+    class Program
+    {
+
+        static int search(int[] array0, int number)
+        {
+            for (int i = 0; i < array0.Length; i++)
+            {
+                if (array0[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введи кол-во элементов");
+            n = int.Parse(Console.ReadLine());
+            int[] array1 = new int[n];
+            Console.WriteLine("Введи массив");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("Вывод массива");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write(array1[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Введи элемент, индекс которого хочешь найти");
+            int element = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Индекс искомого элемента: " + search(array1, element));
+
+
+
+
+        }
+    }
+}
---
+++ Методы/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Методы
+{
+    class Program
+    {
+        static void output(string str, uint n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(str + " ");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string str;
+                uint n;
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("Введи символы");
+                    str = Console.ReadLine();
+                    Console.WriteLine("Введи количество повторений");
+                    n = uint.Parse(Console.ReadLine());
+                }
+
+                catch(Exception)
+                {
+                    Console.WriteLine("Вводи правильно");
+                    Console.ReadLine();
+                    continue;
+                }
+                
+                Program.output(str, n);
+                Console.ReadLine();
+
+            }
+           
+        }
+    }
+}
